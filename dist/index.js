eca74661333d81135f85d693e7e9bb187a8f5224
hehe1111 <2908749709@qq.com>
2020-12-09 16:32:37 +0800

    fix: 修复棋子在边线时显示不完全的问题

--- dist/index.js
+++ dist/index.js
@@ -1,35 +1,32 @@
 "use strict";
 
+var BOARD_AREA_SIZE = 540;
+var COLUMNS = 18; // 设置网格大小
+
+var GRID_SIZE = BOARD_AREA_SIZE / COLUMNS;
+var OFFSET = GRID_SIZE / 2;
+var canvas = document.getElementById('canvas');
+canvas.width = BOARD_AREA_SIZE + OFFSET * 2;
+canvas.height = BOARD_AREA_SIZE + OFFSET * 2;
+var ctx = canvas.getContext('2d');
 drawGoBoard();
 /* === 工具函数 === */
 
 function drawGoBoard() {
-  /*
-   * 1. 设置网格的大小，GRID_SIZE 用于确定网格之中的线之间的间隔
-   * 2. 采用遍历的方式，绘画 x、y 轴的线条
-   */
-  var BOARD_SIZE = 540;
-  var COLUMNS = 18;
-  var canvas = document.getElementById('canvas');
-  canvas.width = BOARD_SIZE;
-  canvas.height = BOARD_SIZE;
-  var ctx = canvas.getContext('2d'); // 1. 设置网格大小
-
-  var GRID_SIZE = BOARD_SIZE / COLUMNS; // 2. 绘制 x、y 轴线
-
+  // 绘制 x、y 轴线
   for (var i = 0; i < COLUMNS + 1; i++) {
     ctx.beginPath();
-    ctx.moveTo(0, GRID_SIZE * i - 0.5); // - 0.5 是为了解决像素模糊问题
+    ctx.moveTo(OFFSET, toLength(i) - 0.5); // - 0.5 是为了解决像素模糊问题
 
-    ctx.lineTo(BOARD_SIZE, GRID_SIZE * i - 0.5);
+    ctx.lineTo(BOARD_AREA_SIZE + OFFSET, toLength(i) - 0.5);
     ctx.stroke();
     ctx.closePath();
   }
 
   for (var j = 0; j < COLUMNS + 1; j++) {
     ctx.beginPath();
-    ctx.moveTo(GRID_SIZE * j - 0.5, 0);
-    ctx.lineTo(GRID_SIZE * j - 0.5, BOARD_SIZE);
+    ctx.moveTo(toLength(j) - 0.5, OFFSET);
+    ctx.lineTo(toLength(j) - 0.5, BOARD_AREA_SIZE + OFFSET);
     ctx.stroke();
     ctx.closePath();
   } // 绘制星元、天元
@@ -39,9 +36,13 @@ function drawGoBoard() {
     ;
     [1 / 6, 1 / 2, 5 / 6].map(function (y) {
       ctx.beginPath();
-      ctx.arc(x * BOARD_SIZE, y * BOARD_SIZE, GRID_SIZE / 8, 0, 2 * Math.PI);
+      ctx.arc(x * BOARD_AREA_SIZE + OFFSET, y * BOARD_AREA_SIZE + OFFSET, GRID_SIZE / 8, 0, 2 * Math.PI);
       ctx.fill();
       ctx.closePath();
     });
   });
+}
+
+function toLength(x) {
+  return GRID_SIZE * x + OFFSET;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,31 @@
+const BOARD_AREA_SIZE = 540
+const COLUMNS = 18
+// 设置网格大小
+const GRID_SIZE = BOARD_AREA_SIZE / COLUMNS
+const OFFSET = GRID_SIZE / 2
+
+const canvas = document.getElementById('canvas')! as HTMLCanvasElement
+canvas.width = BOARD_AREA_SIZE + OFFSET * 2
+canvas.height = BOARD_AREA_SIZE + OFFSET * 2
+const ctx = canvas.getContext('2d')!
+
 drawGoBoard()
 
 /* === 工具函数 === */
 
 function drawGoBoard(): void {
-  /*
-   * 1. 设置网格的大小，GRID_SIZE 用于确定网格之中的线之间的间隔
-   * 2. 采用遍历的方式，绘画 x、y 轴的线条
-   */
-
-  const BOARD_SIZE = 540
-  const COLUMNS = 18
-  const canvas = document.getElementById('canvas')! as HTMLCanvasElement
-  canvas.width = BOARD_SIZE
-  canvas.height = BOARD_SIZE
-
-  const ctx = canvas.getContext('2d')!
-
-  // 1. 设置网格大小
-  const GRID_SIZE = BOARD_SIZE / COLUMNS
-
-  // 2. 绘制 x、y 轴线
+  // 绘制 x、y 轴线
   for (let i = 0; i < COLUMNS + 1; i++) {
     ctx.beginPath()
-    ctx.moveTo(0, GRID_SIZE * i - 0.5) // - 0.5 是为了解决像素模糊问题
-    ctx.lineTo(BOARD_SIZE, GRID_SIZE * i - 0.5)
+    ctx.moveTo(OFFSET, toLength(i) - 0.5) // - 0.5 是为了解决像素模糊问题
+    ctx.lineTo(BOARD_AREA_SIZE + OFFSET, toLength(i) - 0.5)
     ctx.stroke()
     ctx.closePath()
   }
   for (let j = 0; j < COLUMNS + 1; j++) {
     ctx.beginPath()
-    ctx.moveTo(GRID_SIZE * j - 0.5, 0)
-    ctx.lineTo(GRID_SIZE * j - 0.5, BOARD_SIZE)
+    ctx.moveTo(toLength(j) - 0.5, OFFSET)
+    ctx.lineTo(toLength(j) - 0.5, BOARD_AREA_SIZE + OFFSET)
     ctx.stroke()
     ctx.closePath()
   }
@@ -40,9 +35,19 @@ function drawGoBoard(): void {
     [1 / 6, 1 / 2, 5 / 6].map(x => {
       ;[1 / 6, 1 / 2, 5 / 6].map(y => {
         ctx.beginPath()
-        ctx.arc(x * BOARD_SIZE, y * BOARD_SIZE, GRID_SIZE / 8, 0, 2 * Math.PI)
+        ctx.arc(
+          x * BOARD_AREA_SIZE + OFFSET,
+          y * BOARD_AREA_SIZE + OFFSET,
+          GRID_SIZE / 8,
+          0,
+          2 * Math.PI
+        )
         ctx.fill()
         ctx.closePath()
       })
     })
+}
+
+function toLength(x: number): number {
+  return GRID_SIZE * x + OFFSET
 }
