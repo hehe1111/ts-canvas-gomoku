531e2b5f7135b15c70ef0813211fa03235d6f1f5
hehe1111 <2908749709@qq.com>
2020-12-09 22:51:51 +0800

    feat: 完成计算机计算最优落点函数

--- dist/index.js
+++ dist/index.js
@@ -9,7 +9,7 @@ var goGoard = document.getElementById('canvas');
 goGoard.width = BOARD_AREA_SIZE + OFFSET * 2;
 goGoard.height = BOARD_AREA_SIZE + OFFSET * 2;
 var ctx = goGoard.getContext('2d');
-var me = true;
+var user = true;
 var occupied = [];
 
 for (var i = 0; i < LINES; i++) {
@@ -20,8 +20,18 @@ for (var i = 0; i < LINES; i++) {
   }
 }
 
+var _calculateWins = calculateWins(),
+    waysToWin = _calculateWins.waysToWin,
+    wins = _calculateWins.wins; // 用户在各种获胜方法中已经下了多少手
+
+
+var userWins = Array(waysToWin).fill(0); // 电脑在各种获胜方法中已经下了多少手
+
+var computerWins = Array(waysToWin).fill(0); // 游戏是否结束
+
+var over = false;
 drawGoBoard();
-goGoard.addEventListener('click', onClickGoBoard);
+goGoard.addEventListener('click', onUserClick);
 /* === 工具函数 === */
 
 function drawGoBoard() {
@@ -60,7 +70,7 @@ function toLength(x) {
   return GRID_SIZE * x + OFFSET;
 }
 
-function oneStep(x, y, me) {
+function oneStep(x, y, user) {
   ctx.beginPath();
   ctx.arc(toLength(x), toLength(y), GRID_SIZE / 2, 0, 2 * Math.PI);
   var gradient = ctx.createRadialGradient( // 外圆
@@ -68,7 +78,7 @@ function oneStep(x, y, me) {
   toLength(y) - 2, GRID_SIZE / 2, // 内圆
   toLength(x) + 2, toLength(y) - 2, GRID_SIZE / 2 / 8);
 
-  if (me) {
+  if (user) {
     gradient.addColorStop(0, '#0a0a0a');
     gradient.addColorStop(1, '#636766');
   } else {
@@ -81,11 +91,207 @@ function oneStep(x, y, me) {
   ctx.closePath();
 }
 
-function onClickGoBoard(event) {
+function calculateWins() {
+  // 获胜方法总数
+  var waysToWin = 0; // 获胜方法数组
+
+  var wins = [];
+
+  for (var _i2 = 0; _i2 < LINES; _i2++) {
+    wins[_i2] = [];
+
+    for (var _j2 = 0; _j2 < LINES; _j2++) {
+      wins[_i2][_j2] = [];
+    }
+  } // 横线获胜方法
+
+
+  for (var _i3 = 0; _i3 < LINES; _i3++) {
+    for (var _j3 = 0; _j3 < LINES - 4; _j3++) {
+      for (var k = 0; k < 5; k++) {
+        wins[_i3][_j3 + k][waysToWin] = true;
+      }
+
+      waysToWin += 1;
+    }
+  } // 竖线获胜方法
+
+
+  for (var _i4 = 0; _i4 < LINES - 4; _i4++) {
+    for (var _j4 = 0; _j4 < LINES; _j4++) {
+      for (var _k = 0; _k < 5; _k++) {
+        wins[_i4 + _k][_j4][waysToWin] = true;
+      }
+
+      waysToWin += 1;
+    }
+  } // 左上到右下获胜方法
+
+
+  for (var _i5 = 0; _i5 < LINES - 4; _i5++) {
+    for (var _j5 = 0; _j5 < LINES - 4; _j5++) {
+      for (var _k2 = 0; _k2 < 5; _k2++) {
+        wins[_j5 + _k2][_i5 + _k2][waysToWin] = true;
+      }
+
+      waysToWin += 1;
+    }
+  } // 右上到左下获胜方法
+
+
+  for (var _i6 = 0; _i6 < LINES - 4; _i6++) {
+    for (var _j6 = LINES - 1; _j6 > 3; _j6--) {
+      for (var _k3 = 0; _k3 < 5; _k3++) {
+        wins[_i6 + _k3][_j6 - _k3][waysToWin] = true;
+      }
+
+      waysToWin += 1;
+    }
+  }
+
+  return {
+    waysToWin: waysToWin,
+    wins: wins
+  };
+}
+
+function onUserClick(event) {
+  if (over || !user) return;
   var x = Math.floor(event.offsetX / GRID_SIZE);
   var y = Math.floor(event.offsetY / GRID_SIZE);
   if (occupied[x][y]) return;
-  oneStep(x, y, me);
-  occupied[x][y] = me ? 1 : 2;
-  me = !me;
+  oneStep(x, y, user);
+  occupied[x][y] = 1;
+  checkWin({
+    x: x,
+    y: y,
+    isUser: true
+  });
+}
+
+function computersRound() {
+  var max = 0;
+  var maxX = 0;
+  var maxY = 0;
+  var userScores = [];
+  var computerScores = [];
+
+  for (var _i7 = 0; _i7 < LINES; _i7++) {
+    userScores[_i7] = [];
+    computerScores[_i7] = [];
+
+    for (var _j7 = 0; _j7 < LINES; _j7++) {
+      userScores[_i7][_j7] = 0;
+      computerScores[_i7][_j7] = 0;
+    }
+  } // 找出最有价值的点（棋子的最优落点）
+
+
+  for (var _i8 = 0; _i8 < LINES; _i8++) {
+    for (var _j8 = 0; _j8 < LINES; _j8++) {
+      if (occupied[_i8][_j8]) continue; // 如果该点可落子
+
+      for (var k = 0; k < waysToWin; k++) {
+        // 如果该点是某个胜法的其中一个点。如果一个点出现在多种胜法里，则分数会进行累加
+        if (wins[_i8][_j8][k]) {
+          // 根据用户在该种胜法中已经下了多少手，给予不同权重，便于拦截用户
+          switch (userWins[k]) {
+            case 1:
+              userScores[_i8][_j8] += 10;
+              break;
+
+            case 2:
+              userScores[_i8][_j8] += 100;
+              break;
+
+            case 3:
+              userScores[_i8][_j8] += 1000;
+              break;
+
+            case 4:
+              userScores[_i8][_j8] += 10000;
+              break;
+          } // 根据电脑在该种胜法中已经下了多少手，给予不同权重，便于电脑取胜
+
+
+          switch (computerWins[k]) {
+            case 1:
+              computerScores[_i8][_j8] += 20;
+              break;
+
+            case 2:
+              computerScores[_i8][_j8] += 200;
+              break;
+
+            case 3:
+              computerScores[_i8][_j8] += 2000;
+              break;
+
+            case 4:
+              computerScores[_i8][_j8] += 20000;
+              break;
+          }
+        }
+      } // 找出 userScores 和 computerScores 两者中最优的点
+
+
+      if (userScores[_i8][_j8] > max) {
+        max = userScores[_i8][_j8];
+        maxX = _i8;
+        maxY = _j8;
+      } else if (userScores[_i8][_j8] === max) {
+        if (computerScores[_i8][_j8] > computerScores[maxX][maxY]) {
+          maxX = _i8;
+          maxY = _j8;
+        }
+      }
+
+      if (computerScores[_i8][_j8] > max) {
+        max = computerScores[_i8][_j8];
+        maxX = _i8;
+        maxY = _j8;
+      } else if (computerScores[_i8][_j8] === max) {
+        if (userScores[_i8][_j8] > userScores[maxX][maxY]) {
+          maxX = _i8;
+          maxY = _j8;
+        }
+      }
+    }
+  }
+
+  oneStep(maxX, maxY, false);
+  occupied[maxX][maxY] = 2;
+  checkWin({
+    x: maxX,
+    y: maxY,
+    isUser: false
+  });
+}
+
+function checkWin(_ref) {
+  var x = _ref.x,
+      y = _ref.y,
+      isUser = _ref.isUser;
+  var willWin = isUser ? userWins : computerWins;
+  var willLose = isUser ? computerWins : userWins;
+
+  for (var k = 0; k < waysToWin; k++) {
+    if (wins[x][y][k]) {
+      willWin[k] += 1;
+      willLose[k] = 6; // 另一方永远不可能再以这种赢法获胜
+
+      if (willWin[k] === 5) {
+        setTimeout(function () {
+          window.alert("".concat(isUser ? '你' : '计算机', "\u8D62\u4E86"));
+          over = true;
+        }, 0);
+      }
+    }
+  }
+
+  if (!over) {
+    var _user = user;
+    user = !user;
+    _user && computersRound();
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ goGoard.width = BOARD_AREA_SIZE + OFFSET * 2
 goGoard.height = BOARD_AREA_SIZE + OFFSET * 2
 const ctx = goGoard.getContext('2d')!
 
-let me: boolean = true
+let user: boolean = true
 const occupied: number[][] = []
 for (let i = 0; i < LINES; i++) {
   occupied[i] = []
@@ -18,8 +18,16 @@ for (let i = 0; i < LINES; i++) {
   }
 }
 
+const { waysToWin, wins } = calculateWins()
+// 用户在各种获胜方法中已经下了多少手
+const userWins: number[] = Array(waysToWin).fill(0)
+// 电脑在各种获胜方法中已经下了多少手
+const computerWins: number[] = Array(waysToWin).fill(0)
+// 游戏是否结束
+let over = false
+
 drawGoBoard()
-goGoard.addEventListener('click', onClickGoBoard)
+goGoard.addEventListener('click', onUserClick)
 
 /* === 工具函数 === */
 
@@ -62,7 +70,7 @@ function toLength(x: number): number {
   return GRID_SIZE * x + OFFSET
 }
 
-function oneStep(x: number, y: number, me: boolean): void {
+function oneStep(x: number, y: number, user: boolean): void {
   ctx.beginPath()
   ctx.arc(toLength(x), toLength(y), GRID_SIZE / 2, 0, 2 * Math.PI)
   const gradient = ctx.createRadialGradient(
@@ -75,7 +83,7 @@ function oneStep(x: number, y: number, me: boolean): void {
     toLength(y) - 2,
     GRID_SIZE / 2 / 8
   )
-  if (me) {
+  if (user) {
     gradient.addColorStop(0, '#0a0a0a')
     gradient.addColorStop(1, '#636766')
   } else {
@@ -87,11 +95,174 @@ function oneStep(x: number, y: number, me: boolean): void {
   ctx.closePath()
 }
 
-function onClickGoBoard(event: MouseEvent): void {
+function calculateWins() {
+  // 获胜方法总数
+  let waysToWin = 0
+  // 获胜方法数组
+  const wins: boolean[][][] = []
+  for (let i = 0; i < LINES; i++) {
+    wins[i] = []
+    for (let j = 0; j < LINES; j++) {
+      wins[i][j] = []
+    }
+  }
+  // 横线获胜方法
+  for (let i = 0; i < LINES; i++) {
+    for (let j = 0; j < LINES - 4; j++) {
+      for (let k = 0; k < 5; k++) {
+        wins[i][j + k][waysToWin] = true
+      }
+      waysToWin += 1
+    }
+  }
+  // 竖线获胜方法
+  for (let i = 0; i < LINES - 4; i++) {
+    for (let j = 0; j < LINES; j++) {
+      for (let k = 0; k < 5; k++) {
+        wins[i + k][j][waysToWin] = true
+      }
+      waysToWin += 1
+    }
+  }
+  // 左上到右下获胜方法
+  for (let i = 0; i < LINES - 4; i++) {
+    for (let j = 0; j < LINES - 4; j++) {
+      for (let k = 0; k < 5; k++) {
+        wins[j + k][i + k][waysToWin] = true
+      }
+      waysToWin += 1
+    }
+  }
+  // 右上到左下获胜方法
+  for (let i = 0; i < LINES - 4; i++) {
+    for (let j = LINES - 1; j > 3; j--) {
+      for (let k = 0; k < 5; k++) {
+        wins[i + k][j - k][waysToWin] = true
+      }
+      waysToWin += 1
+    }
+  }
+
+  return { waysToWin, wins }
+}
+
+function onUserClick(event: MouseEvent): void {
+  if (over || !user) return
+
   const x = Math.floor(event.offsetX / GRID_SIZE)
   const y = Math.floor(event.offsetY / GRID_SIZE)
   if (occupied[x][y]) return
-  oneStep(x, y, me)
-  occupied[x][y] = me ? 1 : 2
-  me = !me
+  oneStep(x, y, user)
+  occupied[x][y] = 1
+  checkWin({ x, y, isUser: true })
+}
+
+function computersRound(): void {
+  let max = 0
+  let maxX = 0
+  let maxY = 0
+  const userScores: number[][] = []
+  const computerScores: number[][] = []
+  for (let i = 0; i < LINES; i++) {
+    userScores[i] = []
+    computerScores[i] = []
+    for (let j = 0; j < LINES; j++) {
+      userScores[i][j] = 0
+      computerScores[i][j] = 0
+    }
+  }
+
+  // 找出最有价值的点（棋子的最优落点）
+  for (let i = 0; i < LINES; i++) {
+    for (let j = 0; j < LINES; j++) {
+      if (occupied[i][j]) continue
+
+      // 如果该点可落子
+      for (let k = 0; k < waysToWin; k++) {
+        // 如果该点是某个胜法的其中一个点。如果一个点出现在多种胜法里，则分数会进行累加
+        if (wins[i][j][k]) {
+          // 根据用户在该种胜法中已经下了多少手，给予不同权重，便于拦截用户
+          switch (userWins[k]) {
+            case 1:
+              userScores[i][j] += 10
+              break
+            case 2:
+              userScores[i][j] += 100
+              break
+            case 3:
+              userScores[i][j] += 1000
+              break
+            case 4:
+              userScores[i][j] += 10000
+              break
+          }
+
+          // 根据电脑在该种胜法中已经下了多少手，给予不同权重，便于电脑取胜
+          switch (computerWins[k]) {
+            case 1:
+              computerScores[i][j] += 20
+              break
+            case 2:
+              computerScores[i][j] += 200
+              break
+            case 3:
+              computerScores[i][j] += 2000
+              break
+            case 4:
+              computerScores[i][j] += 20000
+              break
+          }
+        }
+      }
+
+      // 找出 userScores 和 computerScores 两者中最优的点
+      if (userScores[i][j] > max) {
+        max = userScores[i][j]
+        maxX = i
+        maxY = j
+      } else if (userScores[i][j] === max) {
+        if (computerScores[i][j] > computerScores[maxX][maxY]) {
+          maxX = i
+          maxY = j
+        }
+      }
+      if (computerScores[i][j] > max) {
+        max = computerScores[i][j]
+        maxX = i
+        maxY = j
+      } else if (computerScores[i][j] === max) {
+        if (userScores[i][j] > userScores[maxX][maxY]) {
+          maxX = i
+          maxY = j
+        }
+      }
+    }
+  }
+
+  oneStep(maxX, maxY, false)
+  occupied[maxX][maxY] = 2
+  checkWin({ x: maxX, y: maxY, isUser: false })
+}
+
+function checkWin({ x, y, isUser }: { x: number; y: number; isUser: boolean }) {
+  const willWin = isUser ? userWins : computerWins
+  const willLose = isUser ? computerWins : userWins
+  for (let k = 0; k < waysToWin; k++) {
+    if (wins[x][y][k]) {
+      willWin[k] += 1
+      willLose[k] = 6 // 另一方永远不可能再以这种赢法获胜
+      if (willWin[k] === 5) {
+        setTimeout(() => {
+          window.alert(`${isUser ? '你' : '计算机'}赢了`)
+          over = true
+        }, 0)
+      }
+    }
+  }
+
+  if (!over) {
+    const _user = user
+    user = !user
+    _user && computersRound()
+  }
 }
